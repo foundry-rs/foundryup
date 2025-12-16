5ab91fe58bcd0b9832214a748cf5e22ac9263b40
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-12-16 05:17:18 +0100

    ci

--- src/process.rs
+++ src/process.rs
@@ -1,23 +1,21 @@
-use crate::config::Config;
-use eyre::{Result, bail};
+use crate::{config::Config, warn};
+use eyre::Result;
 use sysinfo::System;
 
 pub(crate) fn check_bins_in_use(config: &Config) -> Result<()> {
-    // Skip in CI since there are random processes with similar names.
-    if std::env::var_os("CI").is_some() {
-        return Ok(());
-    }
-
     let bins = config.bins(None);
     let mut sys = System::new();
     sys.refresh_processes(sysinfo::ProcessesToUpdate::All, true);
 
-    for &bin in bins {
-        for proc in sys.processes_by_name(bin.as_ref()) {
-            let name = &*proc.name().to_string_lossy();
-            if name == bin || name.ends_with(&*format!("/{bin}")) {
-                bail!("'{name}' is currently running, please stop the process and try again");
-            }
+    for (pid, proc) in sys.processes() {
+        if let Some(exe) = proc.exe().or_else(|| proc.cmd().first().map(AsRef::as_ref))
+            && let Some(exe_fname) = exe.file_name()
+            && let Some(exe_fname) = exe_fname.to_str()
+            && let Some(bin) = bins.iter().find(|&&bin| exe_fname.starts_with(bin))
+        {
+            warn(&format!(
+                "'{bin}' is currently running (PID: {pid}), please stop the process and try again"
+            ));
         }
     }
 
