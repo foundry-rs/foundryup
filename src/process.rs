7300c66c499af5713ddc3e72d92dfa17f76723a6
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-12-16 04:19:06 +0100

    fix: bin check

--- src/process.rs
+++ src/process.rs
@@ -7,10 +7,12 @@ pub(crate) fn check_bins_in_use(config: &Config) -> Result<()> {
     let mut sys = System::new();
     sys.refresh_processes(sysinfo::ProcessesToUpdate::All, true);
 
-    for bin in bins {
-        if let Some(process) = sys.processes_by_name(bin.as_ref()).next() {
-            let name = process.name().to_string_lossy();
-            bail!("'{name}' is currently running, please stop the process and try again");
+    for &bin in bins {
+        for proc in sys.processes_by_name(bin.as_ref()) {
+            let name = &*proc.name().to_string_lossy();
+            if name == bin || name.ends_with(&*format!("/{bin}")) {
+                bail!("'{name}' is currently running, please stop the process and try again");
+            }
         }
     }
 
