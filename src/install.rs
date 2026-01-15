50532e6dbd81d88ca6bcf623db224b7b4e318461
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-01-15 16:26:33 +0000

    feat: symlink foundry binaries instead of copying on Unix

--- src/install.rs
+++ src/install.rs
@@ -480,13 +480,10 @@ pub(crate) fn use_version(config: &Config, version: &str) -> Result<()> {
             fs::remove_file(&dest)?;
         }
 
-        fs::copy(&src, &dest)?;
-
         #[cfg(unix)]
-        {
-            use std::os::unix::fs::PermissionsExt;
-            fs::set_permissions(&dest, std::fs::Permissions::from_mode(0o755))?;
-        }
+        std::os::unix::fs::symlink(&src, &dest)?;
+        #[cfg(windows)]
+        fs::copy(&src, &dest)?;
 
         match get_bin_version(&dest) {
             Ok(v) => say(&format!("use - {v}")),
